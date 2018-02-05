69feb3a45f1f45cd23527f5f2ba55d21603b9c30
Robbyro89 <gallery.laroche.id@gmail.com>
2018-02-05 15:57:51 +0800

    initialize Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/show'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
 root 'static_pages#home'
 
 end
